af0565dbc7a1c4a77635283da9a8a363e3e1bdae
Zaden Connell <zaden.connell@gmail.com>
2022-05-22 16:34:27 -0400

    Initial Commit

---
+++ MyFitnessPal-SwiftUI/MyFitnessPal_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitnessPal_SwiftUIApp.swift
+//  MyFitnessPal-SwiftUI
+//
+//  Created by Zaden Connell on 5/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFitnessPal_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
