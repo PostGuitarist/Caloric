6582d6479c8097c05e3eeee81d20cef7f6af49c6
Zaden Connell <zaden.connell@gmail.com>
2022-06-27 16:38:41 -0400

    File updates

---
+++ Tracked/Components/FoodRow.swift
@@ -0,0 +1,20 @@
+//
+//  FoodRow.swift
+//  Tracked
+//
+//  Created by Zaden Connell on 6/27/22.
+//
+
+import SwiftUI
+
+struct FoodRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FoodRow_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodRow()
+    }
+}
---
+++ Tracked/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Tracked
+//
+//  Created by Zaden Connell on 6/27/22.
+//
+
+import Foundation
